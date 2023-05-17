45a97b817178f5672362f6e00d245af06ffdebf1
Slinky <callum.epeel@gmail.com>
2023-05-17 14:22:41 +0800

    Project added and GUI started

---
+++ TubeData/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TubeData
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            toolStrip1 = new ToolStrip();
+            newToolStripButton = new ToolStripButton();
+            openToolStripButton = new ToolStripButton();
+            saveToolStripButton = new ToolStripButton();
+            printToolStripButton = new ToolStripButton();
+            toolStripSeparator = new ToolStripSeparator();
+            helpToolStripButton = new ToolStripButton();
+            txtDirectoryPath = new TextBox();
+            FolderDirectoryBtn = new Button();
+            treeView1 = new TreeView();
+            imageList1 = new ImageList(components);
+            progressBar1 = new ProgressBar();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            toolTip1 = new ToolTip(components);
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.BackColor = SystemColors.Control;
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { newToolStripButton, openToolStripButton, saveToolStripButton, printToolStripButton, toolStripSeparator, helpToolStripButton });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1243, 27);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // newToolStripButton
+            // 
+            newToolStripButton.Image = (Image)resources.GetObject("newToolStripButton.Image");
+            newToolStripButton.ImageTransparentColor = Color.Magenta;
+            newToolStripButton.Name = "newToolStripButton";
+            newToolStripButton.Size = new Size(63, 24);
+            newToolStripButton.Text = "&New";
+            // 
+            // openToolStripButton
+            // 
+            openToolStripButton.Image = (Image)resources.GetObject("openToolStripButton.Image");
+            openToolStripButton.ImageTransparentColor = Color.Magenta;
+            openToolStripButton.Name = "openToolStripButton";
+            openToolStripButton.Size = new Size(69, 24);
+            openToolStripButton.Text = "&Open";
+            // 
+            // saveToolStripButton
+            // 
+            saveToolStripButton.Image = (Image)resources.GetObject("saveToolStripButton.Image");
+            saveToolStripButton.ImageTransparentColor = Color.Magenta;
+            saveToolStripButton.Name = "saveToolStripButton";
+            saveToolStripButton.Size = new Size(64, 24);
+            saveToolStripButton.Text = "&Save";
+            // 
+            // printToolStripButton
+            // 
+            printToolStripButton.Image = (Image)resources.GetObject("printToolStripButton.Image");
+            printToolStripButton.ImageTransparentColor = Color.Magenta;
+            printToolStripButton.Name = "printToolStripButton";
+            printToolStripButton.Size = new Size(63, 24);
+            printToolStripButton.Text = "&Print";
+            // 
+            // toolStripSeparator
+            // 
+            toolStripSeparator.Name = "toolStripSeparator";
+            toolStripSeparator.Size = new Size(6, 27);
+            // 
+            // helpToolStripButton
+            // 
+            helpToolStripButton.Alignment = ToolStripItemAlignment.Right;
+            helpToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            helpToolStripButton.Image = (Image)resources.GetObject("helpToolStripButton.Image");
+            helpToolStripButton.ImageTransparentColor = Color.Magenta;
+            helpToolStripButton.Name = "helpToolStripButton";
+            helpToolStripButton.Size = new Size(29, 24);
+            helpToolStripButton.Text = "He&lp";
+            // 
+            // txtDirectoryPath
+            // 
+            txtDirectoryPath.Location = new Point(12, 97);
+            txtDirectoryPath.Name = "txtDirectoryPath";
+            txtDirectoryPath.Size = new Size(184, 27);
+            txtDirectoryPath.TabIndex = 1;
+            txtDirectoryPath.TextChanged += textBox1_TextChanged;
+            // 
+            // FolderDirectoryBtn
+            // 
+            FolderDirectoryBtn.Location = new Point(202, 97);
+            FolderDirectoryBtn.Name = "FolderDirectoryBtn";
+            FolderDirectoryBtn.Size = new Size(44, 28);
+            FolderDirectoryBtn.TabIndex = 2;
+            FolderDirectoryBtn.Text = "...";
+            FolderDirectoryBtn.UseVisualStyleBackColor = true;
+            FolderDirectoryBtn.Click += FolderDirectoryBtn_Click;
+            // 
+            // treeView1
+            // 
+            treeView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            treeView1.Location = new Point(12, 131);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(234, 497);
+            treeView1.StateImageList = imageList1;
+            treeView1.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "icons8-opened-folder-16.png");
+            imageList1.Images.SetKeyName(1, "slinky.jpg");
+            // 
+            // progressBar1
+            // 
+            progressBar1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            progressBar1.Location = new Point(12, 634);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(234, 12);
+            progressBar1.TabIndex = 4;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // folderBrowserDialog1
+            // 
+            folderBrowserDialog1.HelpRequest += folderBrowserDialog1_HelpRequest;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1243, 658);
+            Controls.Add(progressBar1);
+            Controls.Add(treeView1);
+            Controls.Add(FolderDirectoryBtn);
+            Controls.Add(txtDirectoryPath);
+            Controls.Add(toolStrip1);
+            Name = "Form1";
+            Text = "Form1";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton newToolStripButton;
+        private ToolStripButton openToolStripButton;
+        private ToolStripButton saveToolStripButton;
+        private ToolStripButton printToolStripButton;
+        private ToolStripSeparator toolStripSeparator;
+        private ToolStripButton helpToolStripButton;
+        private TextBox txtDirectoryPath;
+        private Button FolderDirectoryBtn;
+        private TreeView treeView1;
+        private ProgressBar progressBar1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private ImageList imageList1;
+        private ToolTip toolTip1;
+    }
+}
